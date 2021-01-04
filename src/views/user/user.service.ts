9642afe9e443fd002d533de7ad9ce53303a4bc66
wyyshr <2015599782@qq.com>
2021-01-04 11:33:27 +0800

    修复登录失败

--- src/views/user/user.service.ts
+++ src/views/user/user.service.ts
@@ -10,42 +10,48 @@ export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>
-  ){}
-  
+  ) { }
+
   // 登录
-  async login(body: UserType){
-    const user = await this.userRepository.findOne({nickName: body.nickName})
-    if(!user.nickName) await this.userRepository.save({...body, isInMeeting: true})   // 首次使用
-    await this.userRepository.update(user, {...body, isInMeeting: true})
-    return {code: 1, success: true, identity: user.identity}
+  async login(body: UserType) {
+    const user = await this.userRepository.findOne({ nickName: body.nickName })
+    const data = {
+      code: 1,
+      success: true,
+      identity: body.identity
+    }
+    !user ?
+      await this.userRepository.save({ ...body, isInMeeting: true, isHandsUp: false }) :
+      await this.userRepository.update(user, { ...body, isInMeeting: true, isHandsUp: false })
+    return data
   }
 
   // 获取参加会议人员
-  async getInMeetingUser(query: GetInMeetingUserType){
-    const users = await this.userRepository.find({isInMeeting: true})
+  async getInMeetingUser(query: GetInMeetingUserType) {
+    const users = await this.userRepository.find({ isInMeeting: true })
     const others = users.filter(v => v.nickName != query.nickName)
     const audience = []
-      for (let i = 0; i < others.length; i+=9) {
-        audience.push(others.slice(i, i+9))
-      }
-      return {code: 1, data: audience}
+    for (let i = 0; i < others.length; i += 9) {
+      audience.push(others.slice(i, i + 9))
+    }
+    return { code: 1, data: audience }
   }
 
   // 观众举手
-  async handsUp (query: {nickName: string}){
-    const queryArr = {nickName: query.nickName, identity: 2, isHandsUp: false}
+  async handsUp(query: { nickName: string }) {
+    const queryArr = { nickName: query.nickName, identity: 2, isHandsUp: false }
     const user = await this.userRepository.findOne(queryArr)
-    if(!user) return {code: 0, success: false, data: {msg: '举手失败'}}
-    await this.userRepository.update(queryArr, {isHandsUp: true});
-    return {code: 1, success: true, data: {msg: '成功举手'}}
+    if (!user) return { code: 0, success: false, data: { msg: '举手失败' } }
+    await this.userRepository.update(queryArr, { isHandsUp: true });
+    return { code: 1, success: true, data: { msg: '成功举手' } }
   }
 
   // 取消举手
-  async cancleHandsUp(query: {nickName: string}) {
-    const queryArr = {nickName: query.nickName, identity: 2, isHandsUp: true}
+  async cancleHandsUp(query: { nickName: string }) {
+    const queryArr = { nickName: query.nickName, identity: 2, isHandsUp: true }
     const user = await this.userRepository.findOne(queryArr);
-    if(!user) return {code: 0, success: false, data: {msg: '放下失败'}}
-    await this.userRepository.update(queryArr, {isHandsUp: false});
-    return {code: 1, success: true, data: {msg: '成功放下'}}
+    if (!user) return { code: 0, success: false, data: { msg: '放下失败' } }
+    await this.userRepository.update(queryArr, { isHandsUp: false });
+    return { code: 1, success: true, data: { msg: '成功放下' } }
   }
 }
